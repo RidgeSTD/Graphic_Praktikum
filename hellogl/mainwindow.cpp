9af6a2a5892e18887040cf1644268ab0e672fd00
Wenlan Hua <alex_hua@outlook.com>
2018-10-21 14:49:02 +0200

    toolbar, status bar, about menu

--- hellogl/mainwindow.cpp
+++ hellogl/mainwindow.cpp
@@ -9,11 +9,18 @@ MainWindow::MainWindow(QWidget *parent)
 
     exitAction->setShortcut(QKeySequence("Ctrl+q"));
     connect(exitAction, SIGNAL(triggered()), this, SLOT(close()));
-//    here is nothing showed on OSX because  File->Exit collapses with System menu, it's automatically hiden
+    //    here is nothing showed on OSX because  File->Exit collapses with System menu, it's automatically hiden
     setMenuBar(menuBar);
+
+//    a way to write with conveniece function, again, nothing showed because it's taken by OS X System
+    menuBar->addMenu("&Help")->addAction("&About", this, SLOT(showAboutBox()));
 }
 
 MainWindow::~MainWindow()
 {
+}
 
+void MainWindow::showAboutBox()
+{
+    QMessageBox::information(this, "About Hello GL", "Written by 1337 H4Xx0r!");
 }
--- hellogl/mainwindow.h
+++ hellogl/mainwindow.h
@@ -14,6 +14,9 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+public slots:
+    void showAboutBox();
 };
 
 #endif // MAINWINDOW_H
