4b4e407b16d7f2839494f9329688d6fe19d30f81
Wenlan Hua <alex_hua@outlook.com>
2018-10-21 12:50:41 +0200

    add exit file menu and actions

--- hellogl/mainwindow.cpp
+++ hellogl/mainwindow.cpp
@@ -3,6 +3,14 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    QMenuBar *menuBar = new QMenuBar();
+    QMenu *fileMenu = menuBar->addMenu("&File");
+    QAction *exitAction = fileMenu->addAction("E&xit");
+
+    exitAction->setShortcut(QKeySequence("Ctrl+q"));
+    connect(exitAction, SIGNAL(triggered()), this, SLOT(close()));
+//    here is nothing showed on OSX because  File->Exit collapses with System menu, it's automatically hiden
+    setMenuBar(menuBar);
 }
 
 MainWindow::~MainWindow()
--- hellogl/mainwindow.h
+++ hellogl/mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtWidgets>
+#include <QMenuBar>
+#include <QAction>
+#include <QMenu>
 
 class MainWindow : public QMainWindow
 {
